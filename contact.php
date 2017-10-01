26dfc1d95aa75f4fe1e376844e5d47774957ea16
Joshua Inman <JoshuaInman@icloud.com>
2017-10-01 00:22:05 -0400

    Contact page progression.

--- contact.php
+++ contact.php
@@ -1,3 +1,54 @@
+<?php
+// Message vars.
+$msg = "";
+
+// Check for submit.
+if (filter_has_var(INPUT_POST, "submit")) {
+  // Get form data. 
+  $name = htmlspecialchars($_POST["name"]);
+  $email = htmlspecialchars($_POST["email"]);
+  $message = htmlspecialchars($_POST["message"]);
+}
+
+// Form validation.
+if (!empty($name) && !empty($email) && !empty($message)) {
+  // Name, email and message are all filled out.
+  // Check for valid email.
+  if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+    // Failed.
+    $msg = "<p class='alert-error'>Please enter a valid email address.</p>";
+  } else {
+    // Passed.
+    // Recipent's email. 
+    $toEmail = "JoshuaInman@icloud.com"; 
+    $subject = "Contact Request From " . $name;
+    $body = '<h2>Contact Request</h2>
+            <h4>Name</h4><p>' . $name . '</p>
+            <h4>Email</h4><p>' . $email . '</p>
+            <h4>Message</h4><p>' . $message . '</p>
+    ';
+
+    // Email headers. 
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-Type: text/html;charset=UTF-8" . "\r\n";
+
+    // Additional headers. 
+    $headers .= "From: " . $name . "<" . $email . ">" . "\r\n";
+
+    if (mail($toEmail, $subject, $body, $headers)) {
+      // Email sent. 
+      $msg = "<p class='alert-success'>Your email has been sent!</p>";
+    } else {
+      // Failed.
+      $msg = "<p class='alert-error'>An error has occurred, please try again.</p>";
+    }
+  }
+} else {
+  // Name, email and message are not all filled out.
+  $msg = "<p class='alert-error'>Please fill in all fields.</p>";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +62,19 @@
 	<link rel="stylesheet" type="text/css" href="css/responsive.css">
 	<!-- Temporarily removing viewport meta tag. -->
 	<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+  <style>
+  .alert-box {
+    text-align: center;
+  }
+
+  .alert-success {
+    color: green;
+  }
+
+  .alert-error {
+    color: red;
+  }
+  </style>
 </head>
 <body class="fade-in">
   <header class="header-main">
@@ -20,8 +84,8 @@
       <nav class="header-nav">
         <ul>
           <li><a href="index.html">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="contact.html">Contact</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="#">Contact</a></li>
           <li><a href="changelog.html">Changelog</a></li>
         </ul>
       </nav>
@@ -31,18 +95,21 @@
   <div class="contact-container">
     <article class="main-article">
       <h1>Contact Me</h1>
-      <form method="post" action="">
+      <?php if ($msg != ""): ?>
+      <div class="alert-box"><?php echo $msg; ?></div>
+      <?php endif; ?>
+      <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <div>
           <label>Name</label>
-          <input type="text" name="name" value="">
+          <input type="text" name="name" value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
         </div>
         <div>
           <label>Email</label>
-          <input type="text" name="email" value="">
+          <input type="text" name="email" value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
         </div>
         <div>
           <label>Message</label>
-          <textarea name="message"></textarea>
+          <textarea name="message"><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
         </div>
         <br>
         <button type="submit" name="submit">Submit</button>
